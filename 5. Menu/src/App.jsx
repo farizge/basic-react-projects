65bf75d2657485ffed0713e25661765752f51038
farizge <farizghani11@gmail.com>
2023-07-17 21:46:13 +0700

    Menu (Done)

--- 5. Menu/src/App.jsx
+++ 5. Menu/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import items from "./data";
 import Menu from "./Menu";
+import Categories from "./Categories";
 
 const App = () => {
     const [menuData, setMenuData] = useState(items);
-    const categories = new Set(["all", ...items.map((item) => item.category)]);
+    const categories = ["all", ...new Set(items.map((item) => item.category))];
+
+    const filterItems = (category) => {
+        if (category === "all") {
+            return setMenuData(items);
+        }
+        let newArr = items.filter((item) => item.category === category);
+        setMenuData(newArr);
+    };
 
     return (
         <main>
             <div className="menu">
+                <div>
+                    <h1 className="title">Our Menu</h1>
+                    <div className="title-underline"></div>
+                </div>
+                <Categories categories={categories} filterItems={filterItems} />
                 <Menu items={menuData} />
             </div>
         </main>
