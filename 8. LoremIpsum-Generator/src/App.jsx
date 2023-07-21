1b0cd81510488d5c81d682f1b54da8f749ee7568
farizge <farizghani11@gmail.com>
2023-07-21 23:56:42 +0700

    Lorem Ipsum Generator (OK)

--- 8. LoremIpsum-Generator/src/App.jsx
+++ 8. LoremIpsum-Generator/src/App.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import text from "./data";
+import { nanoid } from "nanoid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+    const [count, setCount] = useState(1);
+    const [paragraph, setParagraph] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setParagraph(text.slice(0, count));
+    };
+
+    return (
+        <main>
+            <section className="section-center">
+                <h4>tired of boring lorem ipsum ?</h4>
+                <form className="lorem-form" onSubmit={handleSubmit}>
+                    <label htmlFor="Paragrhaph">Paragraphs :</label>
+                    <input
+                        type="number"
+                        min={1}
+                        max={8}
+                        step={1}
+                        value={count}
+                        onChange={(e) => setCount(e.target.value)}
+                    />
+                    <button className="btn">Generate</button>
+                </form>
+                <div className="lorem-text">
+                    {paragraph.map((p) => (
+                        <p key={nanoid()}>{p}</p>
+                    ))}
+                </div>
+            </section>
+        </main>
+    );
 };
 export default App;
